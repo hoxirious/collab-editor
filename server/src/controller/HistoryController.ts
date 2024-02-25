fe150b7bd27198981ca6f7754a5d224fc4e5cd0a
Hy Huynh <giahy33@gmail.com>
2024-02-24 21:40:53 -0700

    bug: resolve build error

--- server/src/controller/HistoryController.ts
+++ server/src/controller/HistoryController.ts
@@ -9,73 +9,81 @@ import ChangesetService from "../service/ChangesetService";
 import SnapshotService from "../service/SnapshotService";
 import CodeController from "./CodeController";
 
+
+interface HistoryBody {
+    version: string,
+    memberId: string
+    changesets: Changeset[]
+    baseVersion: number
+}
 export default class HistoryController {
-  static async get(ctx: Context) {
-    const codeId = ctx.params.codeId;
-    if (!codeId) {
-      ctx.status = 400;
-      return;
+    static async get(ctx: Context) {
+        const codeId = ctx.params.codeId;
+        if (!codeId) {
+            ctx.status = 400;
+            return;
+        }
+
+        const userMap: { [userId: number]: {} } = {};
+        const changesets = await ChangesetEntity.find({
+            where: { codeId },
+            order: { baseVersion: 'DESC' },
+            take: 1000,
+        });
+        const list: {}[] = [];
+        for (let i = 0; i < changesets.length; i++) {
+            const userId = changesets[i].userId;
+            const user = userMap[userId] || await UserEntity.findOneBy({ id: userId }) || {};
+            userMap[userId] = user;
+            list.push({
+                ...changesets[i],
+                operations: changesets[i].getOperations(),
+                user,
+            });
+        }
+
+        ctx.body = list;
     }
 
-    const userMap: { [userId: number]: {} } = {};
-    const changesets = await ChangesetEntity.find({
-      where: { codeId },
-      order: { baseVersion: 'DESC' },
-      take: 1000,
-    });
-    const list: {}[] = [];
-    for (let i = 0; i < changesets.length; i++) {
-      const userId = changesets[i].userId;
-      const user = userMap[userId] || await UserEntity.findOneBy({ id: userId }) || {};
-      userMap[userId] = user;
-      list.push({
-        ...changesets[i],
-        operations: changesets[i].getOperations(),
-        user,
-      });
+    static async preview(ctx: Context) {
+        const codeId = ctx.params.codeId;
+        const version = Number(ctx.query.version);
+        if (!codeId || !(version >= 0)) {
+            ctx.status = 400;
+            return;
+        }
+
+        const snapshot = await SnapshotService.get(codeId, version);
+        ctx.body = snapshot;
     }
 
-    ctx.body = list;
-  }
 
-  static async preview(ctx: Context) {
-    const codeId = ctx.params.codeId;
-    const version = Number(ctx.query.version);
-    if (!codeId || !(version >= 0)) {
-      ctx.status = 400;
-      return;
+    static async revert(ctx: Context) {
+        const codeId = ctx.params.codeId;
+        const version = Number((<HistoryBody>ctx.request.body).version);
+        const memberId = Number((<HistoryBody>ctx.request.body).memberId);
+        const userId = Number(ctx.cookies.get('user_id') || ctx.query.userId);
+        if (!codeId || !(version >= 0) || !userId || !memberId) {
+            ctx.status = 400;
+            return;
+        }
+
+        const code = await CodeEntity.findOneBy({ codeId });
+        if (!code || version >= code.version) {
+            ctx.status = 400;
+            return;
+        }
+
+        const changesets = await ChangesetService.getByRange(codeId, version, code.version);
+        const operations = invertOperations(getChangesetOperations(changesets));
+
+        (<HistoryBody>ctx.request.body).changesets = [new Changeset(
+            operations,
+            userId,
+            memberId,
+            code.version,
+        )];
+        (<HistoryBody>ctx.request.body).baseVersion = code.version;
+        await CodeController.uploadChangeset(ctx);
     }
-
-    const snapshot = await SnapshotService.get(codeId, version);
-    ctx.body = snapshot;
-  }
-
-  static async revert(ctx: Context) {
-    const codeId = ctx.params.codeId;
-    const version = Number(ctx.request.body.version);
-    const memberId = Number(ctx.request.body.memberId);
-    const userId = Number(ctx.cookies.get('user_id') || ctx.query.userId);
-    if (!codeId || !(version >= 0) || !userId || !memberId) {
-      ctx.status = 400;
-      return;
-    }
-
-    const code = await CodeEntity.findOneBy({ codeId });
-    if (!code || version >= code.version) {
-      ctx.status = 400;
-      return;
-    }
-
-    const changesets = await ChangesetService.getByRange(codeId, version, code.version);
-    const operations = invertOperations(getChangesetOperations(changesets));
-
-    ctx.request.body.changesets = [new Changeset(
-      operations,
-      userId,
-      memberId,
-      code.version,
-    )];
-    ctx.request.body.baseVersion = code.version;
-    await CodeController.uploadChangeset(ctx);
-  }
 }
--- web/vite.config.ts
+++ web/vite.config.ts
@@ -27,7 +27,7 @@ export default defineConfig({
   },
   plugins: [
     react(),
-    monacoEditorPlugin(),
+    monacoEditorPlugin({}),
     checker({ typescript: true }),
     svgr(),
   ],
